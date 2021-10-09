2d8b4345e18e6805b99893671b82061795b87652
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2021-10-09 23:25:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace cs4sn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if( args.Length < 2 )
+            {
+                printHelp();
+                return;
+            }
+
+            if( args[1] == "hallo")
+            {
+                Console.Write("username: ");
+                string username = Console.ReadLine();
+                Console.WriteLine("Hallo {0}!", username);
+            }
+        }
+
+        static void printHelp()
+        {
+            Console.WriteLine(@"
+cs4n hallo      vraag om een gebruikersnaam en zeg hallo terug
+");
+        }
+
+    }
+}
