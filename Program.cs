1623e7aefd97f9970dd6b97dfa08b4200c1939e2
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2021-10-20 07:30:00 +0200

    Nieuw commando: overflow

--- Program.cs
+++ Program.cs
@@ -19,6 +19,9 @@ namespace cs4sn
                 case "console":
                     runConsole();
                     break;
+                case "overflow":
+                    runOverflow();
+                    break;
                 default:
                     Console.WriteLine($"Onbekend commando [{args[0]}].");
                     break;
@@ -31,6 +34,7 @@ namespace cs4sn
 dotnet run              toon deze help-tekst
 dotnet run hallo        vraag om een gebruikersnaam en zeg hallo terug
 dotnet run console      toon statistieken over de huidige console
+dotnet run overflow     toon een demo waarin een geheugenplaats te klein is voor een waarde en overflow plaatsvindt
 ");
         }
 
@@ -52,5 +56,25 @@ dotnet run console      toon statistieken over de huidige console
             Console.WriteLine("Verander de grootte v.h. terminal-venster en run dit commando opnieuw.");
         }
 
+
+        static void runOverflow() {
+            
+            byte dstVar; // een variabele met 1 byte of 8 bits geheugen, dus max. 2^8 = 256
+            short srcVar; // een short heeft 2 bytes of 15 bits geheugen, dus max. 2^16 = 65536
+            int srcVar2; // een int heeft 4 bytes of 32 bits geheugen dus max. 2^32
+            
+            // we kiezen hier 15 als voorbeeld
+            srcVar = 271; // 256 + 15 = 271
+            srcVar2 = 65551; // 65536 + 15 = 65551
+            //// enkele manieren om te casten:
+            //dstVar = checked((byte)srcVar); // werpt System.OverflowException
+            //dstVar = unchecked((byte)srcVar); // zelfde als hieronder (default)
+            dstVar = (byte)srcVar;
+            
+            Console.WriteLine($"bron: {srcVar} [{srcVar.GetType()}] - bestemming: {dstVar} [{dstVar.GetType()}]");
+            
+            dstVar = (byte)srcVar2;
+            Console.WriteLine($"bron: {srcVar2} [{srcVar2.GetType()}] - bestemming: {dstVar} [{dstVar.GetType()}]");
+        }
     }
 }
